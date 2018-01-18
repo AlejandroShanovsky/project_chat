152e55a254479deb5b8babe022ba6fd4fc0ece45
AlejandroShanovsky <shanko2010@ukr.net>
2018-01-18 19:33:27 +0200

    create method tellEveryone

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,3 +1,52 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class ChatServer {
-    
+    ArrayList clientOutputStreams;
+
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket socket;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                socket = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
+                reader = new BufferedReader(isReader);
+            }catch (IOException ex) { ex.printStackTrace(); }
+        }
+
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                }
+            }catch (IOException ex) { ex.printStackTrace(); }
+        }
+    }
+
+    public void go() {
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+                System.out.println("got a connection");
+            }
+
+        }catch (IOException ex) { ex.printStackTrace(); }
+    }
+
+    public void tellEveryone(String message) {
+        
+    }
 }
